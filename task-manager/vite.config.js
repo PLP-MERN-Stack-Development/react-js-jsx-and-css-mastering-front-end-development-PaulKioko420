db10304b3103261c2e1b8447e6d557c876d2a15e
Paul Kioko <matukupaul1@gmail.com>
2025-10-30 14:39:03 +0300

    Set base path for Vite configuration

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -4,4 +4,5 @@ import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: '/react-js-jsx-and-css-mastering-front-end-development-PaulKioko420/',
 });
